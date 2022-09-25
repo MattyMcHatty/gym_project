c64991ce0b8f871b828771909019d6748dec9e02
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-09-25 18:26:55 +0100

    Created a link to a form to add a new member

--- main/controllers/bookings_controller.py
+++ main/controllers/bookings_controller.py
@@ -4,3 +4,8 @@ from models.booking import Booking
 import repositories.booking_repository as booking_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
+
+@bookings_blueprint.route("/bookings")
+def bookings():
+    bookings = booking_repository.select_all()
+    return render_template("bookings/index.html", bookings=bookings)
--- main/controllers/members_controller.py
+++ main/controllers/members_controller.py
@@ -9,3 +9,16 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repository.select_all()
     return render_template("members/index.html", members=members)
+
+@members_blueprint.route("/members/new")
+def new_member():
+    return render_template("/members/new.html")
+
+@members_blueprint.route("/members", methods=["POST"])
+def create_member():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    premium = request.form["premium"]
+    new_member = Member(first_name, last_name, premium, True)
+    member_repository.save(new_member)
+    return redirect("/members")
