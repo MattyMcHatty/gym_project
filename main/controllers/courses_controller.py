4f347bf966ba0214e6d441bb214d07024cb5ab4f
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-09-25 18:41:40 +0100

    Created a link to a form to add a new course

--- main/controllers/courses_controller.py
+++ main/controllers/courses_controller.py
@@ -9,3 +9,17 @@ courses_blueprint = Blueprint("courses", __name__)
 def courses():
     courses = course_repository.select_all()
     return render_template("courses/index.html", courses=courses)
+
+@courses_blueprint.route("/courses/new")
+def new_course():
+    return render_template("/courses/new.html")
+
+@courses_blueprint.route("/courses", methods=["POST"])
+def create_course():
+    name = request.form["name"]
+    description = request.form["description"]
+    date = request.form["date"]
+    capacity = request.form["capacity"]
+    new_course = Course(name, description, date, capacity)
+    course_repository.save(new_course)
+    return redirect("/courses")
