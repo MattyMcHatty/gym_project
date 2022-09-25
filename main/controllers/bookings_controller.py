e76c5bbdef1c7f8177f77a8df1d2a4d95927e108
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-09-25 19:05:14 +0100

    Created a link to a form to create a new booking which populates the form with all members and courses

--- main/controllers/bookings_controller.py
+++ main/controllers/bookings_controller.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.booking import Booking
 import repositories.booking_repository as booking_repository
+import repositories.member_repository as member_repository
+import repositories.course_repository as course_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
 
@@ -9,3 +11,19 @@ bookings_blueprint = Blueprint("bookings", __name__)
 def bookings():
     bookings = booking_repository.select_all()
     return render_template("bookings/index.html", bookings=bookings)
+
+@bookings_blueprint.route("/bookings/new", methods=['GET'])
+def new_booking():
+    members = member_repository.select_all()
+    courses = course_repository.select_all()
+    return render_template("bookings/new.html", members=members, courses=courses)
+
+@bookings_blueprint.route("/bookings", methods=["POST"])
+def create_booking():
+    member_id = request.form['member_id']
+    course_id = request.form['course_id']
+    member = member_repository.select(member_id)
+    course = course_repository.select(course_id)
+    booking = Booking(member, course)
+    booking_repository.save(booking)
+    return redirect("/bookings")
