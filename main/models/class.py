dd7f7da3e967c07f8ae1efc2ae077b6a5e8464e8
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-09-25 11:11:23 +0100

    Created class class model

---
+++ main/models/class.py
@@ -0,0 +1,9 @@
+class Class:
+
+    def __init__(self, name, description, date, capacity, id = None):
+        self.name = name
+        self.description = description
+        self.date = date
+        self.capacity = capacity
+        self.id = id
+
